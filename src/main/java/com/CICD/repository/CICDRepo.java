0e2e08285abd1a1848dc1e062fd3001bdfeec764
ASHWIN KRISHNAN <ASHWIN KRISHNAN@172.17.253.70>
2024-08-28 21:14:42 +0530

    repo

--- src/main/java/com/CICD/repository/CICDRepo.java
+++ src/main/java/com/CICD/repository/CICDRepo.java
@@ -1,9 +1,10 @@
 package com.CICD.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.CICD.entity.Users;
-
+@Repository
 public interface CICDRepo extends JpaRepository<Users, Integer> {
 
 }
