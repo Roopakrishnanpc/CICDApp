52b2f59fafccd096c81fa3ef5ecf5db209a7507c
ASHWIN KRISHNAN <ASHWIN KRISHNAN@LAPTOP-QI585BC8.mshome.net>
2024-08-30 12:20:05 +0530

    commit CICD

--- src/main/java/com/CICD/controller/CICDController.java
+++ src/main/java/com/CICD/controller/CICDController.java
@@ -13,11 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.CICD.entity.Users;
 import com.CICD.service.CICDService;
 
-@Controller
+@RestController
 @RequestMapping("/homepage")
 public class CICDController {
 
