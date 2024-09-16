7b81b8f31e87770e1f99bb5aca19514a1142d149
ASHWIN KRISHNAN <ASHWIN KRISHNAN@LAPTOP-QI585BC8>
2024-09-16 16:47:24 +0530

    cicd update

--- src/main/java/com/cicdapp/service/CICDService.java
+++ src/main/java/com/cicdapp/service/CICDService.java
@@ -2,55 +2,50 @@ package com.cicdapp.service;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.cicdapp.entity.Users;
 import com.cicdapp.exception.UserNotFoundException;
 import com.cicdapp.repository.CICDRepo;
+
 @Service
 public class CICDService {
-@Autowired
-CICDRepo cicdRepo;
-	public List<Users> getAllUsers() {
-		
-		return cicdRepo.findAll();
-	}
-	public Users getAllUsersById(int id) {
-		
-		Optional<Users> optionalUser=cicdRepo.findById(id);
-		//return optionalUser.orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
-		if(optionalUser.isPresent())
-			return optionalUser.get();
-		else
-			throw new UserNotFoundException("User not found with ID: " + id);
-			
-	}
-	public void deleteUsersById(int id) {
-		
-		if(cicdRepo.existsById(id))
-		{
-			cicdRepo.deleteById(id);
-			
-			String result= "Deleted by " +id;
-			System.out.println(result);
-		}
-		else
-			throw new UserNotFoundException("User not found with ID: " + id);
-	}
-	public void deleteAllUsers() {
-		
-		cicdRepo.deleteAll();
-		
-	}
-	public Users addUsers(Users users) {
-		
-		return cicdRepo.save(users);
-	}
-	public Users updateUsersById(int id, Users updatedUser) {
-		
-		 System.out.println("Updating user with ID: " + id);
+
+    private static final Logger logger = LoggerFactory.getLogger(CICDService.class);
+
+    @Autowired
+    private CICDRepo cicdRepo;
+
+    public List<Users> getAllUsers() {
+        return cicdRepo.findAll();
+    }
+
+    public Users getAllUsersById(int id) {
+        Optional<Users> optionalUser = cicdRepo.findById(id);
+        return optionalUser.orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
+    }
+
+    public void deleteUsersById(int id) {
+        if (cicdRepo.existsById(id)) {
+            cicdRepo.deleteById(id);
+            logger.info("Deleted user with ID: {}", id);
+        } else {
+            throw new UserNotFoundException("User not found with ID: " + id);
+        }
+    }
+
+    public void deleteAllUsers() {
+        cicdRepo.deleteAll();
+    }
+
+    public Users addUsers(Users users) {
+        return cicdRepo.save(users);
+    }
+
+    public Users updateUsersById(int id, Users updatedUser) {
+        logger.info("Updating user with ID: {}", id);
         return cicdRepo.findById(id)
                 .map(existingUser -> {
                     existingUser.setName(updatedUser.getName());
@@ -60,7 +55,5 @@ CICDRepo cicdRepo;
                     return cicdRepo.save(existingUser);
                 })
                 .orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
-
-	}
-
+    }
 }
